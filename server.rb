a65c237a685f75bcc051cf7eed49b2a30f752e4c
Andrew Tabudlo <andrew.tabudlo@gmail.com>
2018-04-19 23:44:13 -0400

    project mvp

--- server.rb
+++ server.rb
@@ -1,28 +1,40 @@
 require 'sinatra'
 require 'sinatra/reloader'
-require_relative './classes.rb'
+require 'sendgrid-ruby'
+require './classes.rb'
+include SendGrid
 
+set :friendly_bakery, @friendly_bakery
 
 get '/' do
   erb :index
 end
 
 post '/email' do
-  puts params[:email]
+  template = (erb :htmlemail, :layout => false)
+  from = Email.new(email: 'info@friendlybakery.com')
+  to = Email.new(email: params[:email])
+  subject = 'Friendly Bakery Catalog'
+  content = Content.new(type: 'text/html', value: template)
+  mail = Mail.new(from, subject, to, content)
+
+  sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+  response = sg.client.mail._('send').post(request_body: mail.to_json)
+  puts response.status_code
+  puts response.body
+  # puts response.parsed_body
+  puts response.headers
+  erb :email
 end
 
 get '/:type' do
-  @friendly_bakery = Bakery.new
-
-  # cookies
-  @chocolate_chip = Pastry.new('cookie', 'Chocolate Chip','images/cookies/chocolate_chip.jpg', 'nom nom nom', ['butter', 'sugar', 'brown sugar', 'eggs', 'vanilla extract', 'flour', 'baking soda', 'water', 'salt', 'chocolate chips'], 2, 10)
-  @oatmeal_raisin = Pastry.new('cookie', 'Oatmeal Raisin','images/cookies/oatmeal_raisin.jpg', 'nom nom nom nom', ['butter', 'sugar', 'brown sugar', 'eggs', 'vanilla extract', 'flour', 'baking soda', 'water', 'salt', 'rolled oats'], 3, 10)
-  @peanut_butter_banana = Pastry.new('cookie', 'Peanut Butter Banana','images/cookies/banana.jpg', 'nom nom nom', ['butter', 'sugar', 'brown sugar', 'eggs', 'vanilla extract', 'flour', 'baking soda', 'water', 'salt', 'chocolate chips'], 2, 10)
-  @friendly_bakery.add_pastry('cookie', @chocolate_chip)
-  @friendly_bakery.add_pastry('cookie', @oatmeal_raisin)
-  @friendly_bakery.add_pastry('cookie', @peanut_butter_banana)
-
-  @cookies = @friendly_bakery.cookies
-  erb :pastry
+  @type = params[:type]
   
+  if params[:type] == 'cookies'
+    erb :cookies
+  elsif params[:type] == 'cakes'
+    erb :cakes
+  elsif params[:type] == 'muffins'
+    erb :muffins
+  end
 end
